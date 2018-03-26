c187ca570a7b77274a7906e3595d98620ec8a42a
Daniela Sainz <dasainz18@gmail.com>
2018-03-26 15:20:31 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/loadForm")
+    public String loadFormPage () {
+        return "formtemplate";
+    }
+    @RequestMapping("/processForm")
+    public String loadFromPage (@RequestParam("loginname") String loginvalue, Model model) {
+
+         //System.out.println(login);
+        model.addAttribute("loginval", loginvalue);
+        model.addAttribute("Terry", "My name is Terry");
+     return "confirm";
+    }
+}
